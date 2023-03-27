07069bf94566dfd8ff5036e9d2fcdf0c4b2ac4be
swapnil <swapnilchougule00@gmail.com>
2023-03-27 17:09:04 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Landing from "./components/Landing";
+import Details from "./components/Details";
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
+
+  const [image , setImage] = useState(null)
+
+  const handleImage = (e)=>{
+
+    setImage(URL.createObjectURL( e.target.files[0]))
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-[rgb(36,36,36)]">
+      <Routes>
+        <Route path="/" element={<Landing image={image} handleImage={handleImage} />} />
+        <Route path="/details" element={<Details  image={image} />} />
+      </Routes>
     </div>
   );
 }
